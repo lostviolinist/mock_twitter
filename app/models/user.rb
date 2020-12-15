f4463e26d700abf6facb9b234064b48e6a4b89d2
joyce <lostviolinist@gmail.com>
2020-12-15 10:41:49 +0800

    add validations for name, email and password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
+    before_save { self.email = email.downcase }
     has_many :posts
-    validates :name, presence: true
-    validates :email, presence: true
+    validates :name, presence: true, length: { maximum: 100}
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true, length: { maximum: 100}, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
+    validates :password, presence: true, length: {minimum: 6 }
+    has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_14_160952) do
+ActiveRecord::Schema.define(version: 2020_12_15_023203) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 2020_12_14_160952) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
